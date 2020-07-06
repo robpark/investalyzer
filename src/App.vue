0f7916c5cc18d1682e4b34f8fb07b610884794a9
robpark <robert.d.park@gmail.com>
2020-07-06 11:48:11 -0400

    Links, styling and a footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <v-app>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1>Investalyzer Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-          <v-text-field type="password" label="Password" prepend-icon="mdi-lock" />
-        </v-form>
-      </v-card-text>
-      <v-divider />
-      <v-card-actions>
-        <v-spacer />
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Investalyzer</v-toolbar-title>
+      <v-spacer />
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <v-main>
+      <!-- Login -->
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1>Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+            <v-text-field type="password" label="Password" prepend-icon="mdi-lock" />
+          </v-form>
+        </v-card-text>
+        <v-divider />
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Footer from './components/Footer';
+
 export default {
   name: 'App',
-  components: {},
+  components: { Footer },
   data: () => ({
     //
   })
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-footer color="primary lighten-1" padless>
+    <v-layout wrap align-center>
+      <v-spacer />
+      <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">
+        {{ link }}
+      </v-btn>
+      <v-flex primary lighten-2 py-4 text-center white--text xs12>
+        4 Legs Software &copy;{{ new Date().getFullYear() }}
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+
+  data: () => ({
+    links: ['Home', 'Login']
+  })
+};
+</script>
+
+<style scoped></style>
